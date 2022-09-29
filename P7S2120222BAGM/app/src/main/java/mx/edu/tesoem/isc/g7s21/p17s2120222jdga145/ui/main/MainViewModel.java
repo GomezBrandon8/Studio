83627552f8188186a92a1cc4e5604c841e552ed2
GomezBrandon8 <82806127+GomezBrandon8@users.noreply.github.com>
2022-09-28 23:44:15 -0500

    Add files via upload

---
+++ P7S2120222BAGM/app/src/main/java/mx/edu/tesoem/isc/g7s21/p17s2120222jdga145/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package mx.edu.tesoem.isc.g7s21.p17s2120222jdga145.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
